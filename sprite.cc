18ce2b207bf154c82847eb19a6041f338e54981c
0xBAMA <jon.baker1222@gmail.com>
2021-11-04 20:10:51 -0500

    small changes, remove surfaces

--- sprite.cc
+++ sprite.cc
@@ -2,25 +2,25 @@
 #include "sprite.h"
 
 sprite::sprite() {
-    position = vector2< int >(0, 0);
-    dimensions = vector2< int >(0, 0);
-    rotation = 0;
+  position = vector2< int >(0, 0);
+  dimensions = vector2< int >(0, 0);
+  rotation = 0;
 }
 
 void sprite::loadFromFile( std::string fileName ) {
-    image = IMG_Load("sprite.png");
-    if (image == NULL) std::cout << "Error loading image: " << IMG_GetError() << std::endl;
-    texture = SDL_CreateTextureFromSurface(myRenderer, image);
+  image = IMG_Load( "sprite.png" );
+  if( image == NULL ) std::cout << "Error loading image: " << IMG_GetError() << std::endl;
+  texture = SDL_CreateTextureFromSurface( myRenderer, image );
 }
 
 
 void sprite::draw(){
-    if (texture == nullptr) std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
-    SDL_RenderCopy(myRenderer, texture, NULL, NULL);
+  if( texture == nullptr ) std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
+  SDL_RenderCopy( myRenderer, texture, NULL, NULL );
 }
 
 
 sprite::~sprite(){
-    SDL_DestroyTexture(texture);
-    SDL_FreeSurface(image);
+  SDL_DestroyTexture( texture );
+  SDL_FreeSurface( image );
 }
--- sprite.h
+++ sprite.h
@@ -1,7 +1,6 @@
 #include "vector.h"
 
 #define unixcheck (!defined(_WIN32) && (defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__))))
-
 #if unixcheck
   #include <SDL2/SDL.h>
   #include <SDL2/SDL_image.h>
@@ -26,13 +25,8 @@ private:
 	// sprite parameters
 	vector2< int > position;
 	vector2< int > dimensions;
-
-	float rotation; // degrees/radians tbd
+	float rotation;
 
   SDL_Renderer* myRenderer;
-
-	// sprite contents - populated by stb_image
-	SDL_Surface* image;
 	SDL_Texture* texture;
-
 };

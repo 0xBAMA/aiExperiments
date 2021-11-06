7eddeb5a8080f7abab1d8746b0014a30752d45e7
0xBAMA <jon.baker1222@gmail.com>
2021-11-05 19:16:00 -0500

    some changes to SDF classes

--- SDFs.h
+++ SDFs.h
@@ -1,56 +1,52 @@
 #include "vector.h"
 #include <cmath>
 
-template <class T>
 class SDF {
 public:
-    vector2<T> position;
-    T scale;
-   
-    virtual T distance(vector2<T> p) {
-        //....
-        return 1.0f;
-    }
-
-};
-template <class T>
-class circle : public SDF<T> {
-    using SDF<T>::distance;
-public:
-    circle<T>(vector2<T> pos, T s) {
-        this->position = pos;
-        this->scale = s;
-    }
-
-    T distance(vector2<T> p) {
-        vector2<T> pos = this->position - p;
-        return sqrt(pos.values[0] * pos.values[0] + pos.values[1] * pos.values[1]) - this->scale;
-    }
-
+  virtual float distance( vector2< float > p ) {
+    return 1.0f;
+  }
 };
 
-template <class T>
-class lineSegment : public SDF<T> {
-    using SDF<T>::distance;
+
+
+class circle : public SDF {
 public:
-    lineSegment<T>(vector2<T> pos, vector2<T> pos2, T s) {
-        this->position = pos;
-        position2 = pos2;
-        this->scale = s;
-    }
+  circle( vector2< float > pos, float r ) {
+    this->position = pos;
+    this->radius = r;
+  }
 
-    T distance(vector2<T> p) {
-        vector2<T> AP = p - this->position;
-        vector2<T> AB = position2 - this->position;
-        T mm = dot(AP, AB) / dot(AB,AB);
-        mm = (mm > 1.0) ? 1.0 : (mm < 0.) ? 0.: mm;
-        
-        vector2<T> pos = (this->position + AB*mm) - p;
-
-        return sqrt(pos.values[0] * pos.values[0] + pos.values[1] * pos.values[1]) - this->scale;
-    }
+  float distance( vector2< float > p ) {
+    vector2< float > pos = this->position - p;
+    return sqrt( pos.values[ 0 ] * pos.values[ 0 ] + pos.values[ 1 ] * pos.values[ 1 ] ) - this->scale;
+  }
 
 private:
-    vector2<T> position2;
+  vector2< float > position;
+  float radius;
 };
 
+
+
+class lineSegment : public SDF {
+public:
+  lineSegment( vector2< float > pos, vector2< float > pos2, float s ) {
+    this->position = pos;
+    position2 = pos2;
+    this->scale = s;
+  }
+
+  float distance( vector2< float > p ) {
+    vector2< float > AP = p - this->position;
+    vector2< float > AB = position2 - this->position;
+    float mm = dot(AP, AB) / dot(AB,AB);
+    mm = ( mm > 1.0 ) ? 1.0 : ( mm < 0. ) ? 0.: mm;
+
+    vector2< float > pos = ( this->position + AB * mm ) - p;
+    return sqrt( pos.values[ 0 ] * pos.values[ 0 ] + pos.values[ 1 ] * pos.values[ 1 ] ) - this->scale;
+  }
+
+private:
+  vector2< float > position2;
+};
